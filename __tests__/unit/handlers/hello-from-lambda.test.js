22a4cf11141d36c7caefc3b91a532ff6027192d9
Mihir Parikh <mihirpar@amdocs.com>
2020-10-17 02:24:48 +0530

    'tukka-progress'

--- __tests__/unit/handlers/hello-from-lambda.test.js
+++ __tests__/unit/handlers/hello-from-lambda.test.js
@@ -13,7 +13,7 @@ describe('Test for hello-from-lambda', () => {
                 If you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in hello-from-lambda.js,
                 you should change the following line to `const expectedResult = 'Hello World!';`
         */
-        const expectedResult = 'Hello from Lambda3!';
+        const expectedResult = 'Hello from Lambda4!';
 
         // Compare the result with the expected result
         expect(result).toEqual(expectedResult);
