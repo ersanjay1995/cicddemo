3b87acfb9c2c50669a2f4d0673bb43ac31d01588
Mnp <test@testmnp.com>
2020-10-17 16:20:53 +0000

    Added applied_build_spec.sh

---
+++ applied_build_spec.sh
@@ -0,0 +1,8 @@
+
+  aws codebuild start-build --project-name mnp-t2 --source-version $CODEBUILD_SOURCE_VERSION --buildspec-override $1/buildspec.yml --artifacts-override type=s3,location=$S3_BUCKET,path=$(cut -d/ -f2-4 <<< $CODEBUILD_SOURCE_VERSION)_op |jq -r ".build.id" >$1/bid
+       sleep 15; maxTime=45;
+       until aws codebuild batch-get-builds --ids $(cat $1/bid) | jq -r ".builds[0].buildStatus" | grep -q "SUCCEEDED"; do sleep 1;if [[ $((maxTime--)) -lt 0 ]]; then echo Timedout; exit 1; fi;  done;
+       #aws codebuild batch-get-builds --ids $(cat $1/bid)
+       f3yml=$(aws codebuild batch-get-builds --ids $(cat $1/bid) |  jq -r ".builds[0].artifacts.location")/template-export.yml
+     #  echo $f3yml
+       aws s3 cp s3://${f3yml##*:::} $1/template.yml
