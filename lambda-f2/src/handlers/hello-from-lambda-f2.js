db33b391762e16f34b878ace1b2c26177ab14855
Sanjay Makwana <sanjay.m@latitudetechnolabs.com>
2020-10-18 19:54:34 +0530

    test pipeline

--- lambda-f2/src/handlers/hello-from-lambda-f2.js
+++ lambda-f2/src/handlers/hello-from-lambda-f2.js
@@ -8,6 +8,6 @@ exports.helloFromLambdaHandler = async () => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log(message);
-
+    console.log(message);
     return message;
 };
